6760377f5ef1d9331bfb799a5e00ae405d622f39
Yongzhi Liu <yongzhiliu@Yongzhis-MacBook-Pro.local>
2020-05-02 17:19:33 +0800

    should_say_fizz_when_reported_figure_is_multiple_of_5,should_say_FizzBuzz_when_reported_figure_is_multiple_of_3_and_5

--- src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
@@ -2,8 +2,14 @@ package cn.xpbootcamp.fizzbuzz;
 
 public class FizzBuzz {
     public static String getReportedFigure(int i) {
+        if (i % 3 == 0 && i % 5 == 0)
+            return "FizzBuzz";
+
         if (i % 3 == 0)
             return "Fizz";
+        if (i % 5 == 0)
+            return "Buzz";
+
         return String.valueOf(i);
     }
 }
--- src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
@@ -18,7 +18,20 @@ public class FizzBuzzTest {
         assertEquals("Fizz", FizzBuzz.getReportedFigure(3));
         assertEquals("Fizz", FizzBuzz.getReportedFigure(6));
         assertEquals("Fizz", FizzBuzz.getReportedFigure(9));
-        assertEquals("Fizz", FizzBuzz.getReportedFigure(15));
         assertEquals("Fizz", FizzBuzz.getReportedFigure(21));
+    }
+
+    @Test
+    void should_say_fizz_when_reported_figure_is_multiple_of_5 () {
+        assertEquals("Buzz", FizzBuzz.getReportedFigure(5));
+        assertEquals("Buzz", FizzBuzz.getReportedFigure(10));
+        assertEquals("Buzz", FizzBuzz.getReportedFigure(20));
+        assertEquals("Buzz", FizzBuzz.getReportedFigure(35));
+    }
+
+    @Test
+    void should_say_fizz_when_reported_figure_is_multiple_of_3_and_5 () {
+        assertEquals("FizzBuzz", FizzBuzz.getReportedFigure(15));
+        assertEquals("FizzBuzz", FizzBuzz.getReportedFigure(30));
     }
 }
