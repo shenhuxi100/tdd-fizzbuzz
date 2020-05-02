270d45098891583fb541ac728a9a75a6bd4f9ab6
Yongzhi Liu <yongzhiliu@Yongzhis-MacBook-Pro.local>
2020-05-02 17:08:34 +0800

    should_say_fizz_when_reported_figure_is_multiple_of_3

--- src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
@@ -9,8 +9,16 @@ public class FizzBuzzTest {
 
     @Test
     void should_say_current_number_when_reported_figure_is_multiple_of_1_and_2 () {
-        assertEquals(1, FizzBuzz.getReportedFigure(1));
-        assertEquals(2, FizzBuzz.getReportedFigure(2));
+        assertEquals("1", FizzBuzz.getReportedFigure(1));
+        assertEquals("2", FizzBuzz.getReportedFigure(2));
     }
 
+    @Test
+    void should_say_fizz_when_reported_figure_is_multiple_of_3 () {
+        assertEquals("Fizz", FizzBuzz.getReportedFigure(3));
+        assertEquals("Fizz", FizzBuzz.getReportedFigure(6));
+        assertEquals("Fizz", FizzBuzz.getReportedFigure(9));
+        assertEquals("Fizz", FizzBuzz.getReportedFigure(15));
+        assertEquals("Fizz", FizzBuzz.getReportedFigure(21));
+    }
 }
