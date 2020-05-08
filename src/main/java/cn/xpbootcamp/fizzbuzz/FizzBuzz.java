8e468d605edbbf5769b6c2c0178aab054c6580a1
xiaofeng.zhi <xiaofeng.zhi@staff.smartline.com.au>
2020-05-08 20:49:17 +0800

    refactoring

--- src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class FizzBuzz {
 
-    private static Map fizzBuzzWhizzMap = new HashMap<Integer, String>();
+    private static Map<Integer, String> fizzBuzzWhizzMap = new HashMap();
 
     static {
         fizzBuzzWhizzMap.put(3, "Fizz");
@@ -16,11 +16,11 @@ public class FizzBuzz {
     public static String getReportedFigure(int i) {
         String reportFigure = "";
         if(i % 3 == 0)
-            reportFigure = (String) fizzBuzzWhizzMap.get(Integer.valueOf(3));
+            reportFigure = fizzBuzzWhizzMap.get(3);
         if(i % 5 == 0)
-            reportFigure = reportFigure + (String) fizzBuzzWhizzMap.get(Integer.valueOf(5));
+            reportFigure = reportFigure + fizzBuzzWhizzMap.get(5);
         if(i % 7 == 0)
-            reportFigure = reportFigure + (String) fizzBuzzWhizzMap.get(Integer.valueOf(7));
+            reportFigure = reportFigure + fizzBuzzWhizzMap.get(7);
 
         return reportFigure.isEmpty() ? String.valueOf(i) : reportFigure;
     }
