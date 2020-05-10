cef28519527bb79f90ac5a7cb533005aeb896a5e
Yongzhi Liu <yongzhiliu@Yongzhis-MacBook-Pro.local>
2020-05-10 20:12:27 +0800

    add cases 4, 5, 6, 7

--- src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
@@ -13,15 +13,23 @@ public class FizzBuzz {
         fizzBuzzWhizzMap.put(7, "Whizz");
     }
 
-    public static String getReportedFigure(int i) {
+    public static String getReportedFigure(int number) {
         String reportFigure = "";
-        if(i % 3 == 0)
+        String numberStr = String.valueOf(number);
+        boolean isContains3 = numberStr.contains("3");
+        boolean isContains5 = numberStr.contains("5");
+        boolean isContains7 = numberStr.contains("7");
+
+        if(isContains3 && !isContains5)
+            return fizzBuzzWhizzMap.get(3);
+
+        if(number % 3 == 0 && (!isContains5 || isContains7))
             reportFigure = fizzBuzzWhizzMap.get(3);
-        if(i % 5 == 0)
+        if(number % 5 == 0 && !isContains7)
             reportFigure = reportFigure + fizzBuzzWhizzMap.get(5);
-        if(i % 7 == 0)
+        if(number % 7 == 0)
             reportFigure = reportFigure + fizzBuzzWhizzMap.get(7);
 
-        return reportFigure.isEmpty() ? String.valueOf(i) : reportFigure;
+        return reportFigure.isEmpty() ? String.valueOf(number) : reportFigure;
     }
 }
--- src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
@@ -7,60 +7,81 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
 
     @Test
-    void should_say_current_number_when_reported_figure_is_multiple_of_1_and_2 () {
+    void should_say_current_number_when_reported_figure_is_multiple_of_1_and_2_when_reporting () {
         assertEquals("1", FizzBuzz.getReportedFigure(1));
         assertEquals("2", FizzBuzz.getReportedFigure(2));
     }
 
     @Test
-    void should_say_Fizz_when_reported_figure_is_multiple_of_3 () {
+    void should_say_Fizz_when_reported_figure_is_multiple_of_3_when_reporting () {
         assertEquals("Fizz", FizzBuzz.getReportedFigure(3));
         assertEquals("Fizz", FizzBuzz.getReportedFigure(6));
         assertEquals("Fizz", FizzBuzz.getReportedFigure(9));
     }
 
     @Test
-    void should_say_Buzz_when_reported_figure_is_multiple_of_5 () {
+    void should_say_Buzz_when_reported_figure_is_multiple_of_5_when_reporting () {
         assertEquals("Buzz", FizzBuzz.getReportedFigure(5));
         assertEquals("Buzz", FizzBuzz.getReportedFigure(10));
         assertEquals("Buzz", FizzBuzz.getReportedFigure(20));
     }
 
     @Test
-    void should_say_FizzBuzz_when_reported_figure_is_multiple_of_3_and_5 () {
-        assertEquals("FizzBuzz", FizzBuzz.getReportedFigure(15));
-        assertEquals("FizzBuzz", FizzBuzz.getReportedFigure(30));
+    void should_say_FizzBuzz_when_reported_figure_is_multiple_of_3_and_5_when_reporting () {
+        assertEquals("FizzBuzz", FizzBuzz.getReportedFigure(60));
     }
 
     @Test
-    void should_say_Whizz_when_reported_figure_is_multiple_of_7 () {
+    void should_say_Whizz_when_reported_figure_is_multiple_of_7_when_reporting () {
         assertEquals("Whizz", FizzBuzz.getReportedFigure(7));
         assertEquals("Whizz", FizzBuzz.getReportedFigure(14));
         assertEquals("Whizz", FizzBuzz.getReportedFigure(28));
     }
 
     @Test
-    void should_say_FizzWhizz_when_reported_figure_is_multiple_of_3_and_7 () {
+    void should_say_FizzWhizz_when_reported_figure_is_multiple_of_3_and_7_when_reporting () {
         assertEquals("FizzWhizz", FizzBuzz.getReportedFigure(21));
         assertEquals("FizzWhizz", FizzBuzz.getReportedFigure(42));
-        assertEquals("FizzWhizz", FizzBuzz.getReportedFigure(63));
+
     }
 
     @Test
-    void should_say_BuzzWhizz_when_reported_figure_is_multiple_of_5_and_7 () {
-        assertEquals("BuzzWhizz", FizzBuzz.getReportedFigure(35));
-        assertEquals("BuzzWhizz", FizzBuzz.getReportedFigure(70));
+    void should_say_BuzzWhizz_when_reported_figure_is_multiple_of_5_and_7_when_reporting () {
+        assertEquals("BuzzWhizz", FizzBuzz.getReportedFigure(140));
     }
 
     @Test
-    void should_say_FizzBuzzWhizz_when_reported_figure_is_multiple_of_3_5_and_7 () {
-        assertEquals("FizzBuzzWhizz", FizzBuzz.getReportedFigure(105));
+    void should_say_FizzBuzzWhizz_when_reported_figure_is_multiple_of_3_5_and_7_when_reporting () {
+        assertEquals("FizzBuzzWhizz", FizzBuzz.getReportedFigure(210));
     }
 
     @Test
-    void should_say_FizzBuzzWhizz_when_reported_figure_is_not_multiple_of_3_5_and_7 () {
+    void should_say_number_given_reported_figure_is_not_multiple_of_3_5_and_7_when_reporting () {
         assertEquals("104", FizzBuzz.getReportedFigure(104));
         assertEquals("11", FizzBuzz.getReportedFigure(11));
         assertEquals("52", FizzBuzz.getReportedFigure(52));
+    }
+
+    @Test
+    void should_say_Fizz_given_reported_figure_contains_3_when_reporting () {
+        assertEquals("Fizz", FizzBuzz.getReportedFigure(13));
+        assertEquals("Fizz", FizzBuzz.getReportedFigure(63));
+        assertEquals("Fizz", FizzBuzz.getReportedFigure(30));
+    }
+
+    @Test
+    void should_say_BuzzWhizz_given_reported_figure_contains_5_when_reporting () {
+        assertEquals("BuzzWhizz", FizzBuzz.getReportedFigure(35));
+    }
+
+    @Test
+    void should_say_Fizz_given_reported_figure_contains_7_when_reporting () {
+        assertEquals("Fizz", FizzBuzz.getReportedFigure(75));
+    }
+
+
+    @Test
+    void should_say_Whizz_given_reported_figure_contains_7_and_multiple_of_5_when_reporting () {
+        assertEquals("Whizz", FizzBuzz.getReportedFigure(70));
     }
 }
